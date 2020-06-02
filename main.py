c21ace6c14b3b969dc6f83777e91318727430c6c
leon-liangwu <wuliang0301@hotmail.com>
2020-06-02 14:44:36 +0800

    gpu id

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str, default='MiniImageNet', choices=['miniImageNet', 'tieredImageNet', 'FC100']) # Dataset
     parser.add_argument('--phase', type=str, default='meta_train', choices=['pre_train', 'meta_train', 'meta_eval']) # Phase
     parser.add_argument('--seed', type=int, default=0) # Manual seed for PyTorch, "0" means using random seed
-    parser.add_argument('--gpu', default='1') # GPU id
+    parser.add_argument('--gpu', default='0') # GPU id
     parser.add_argument('--dataset_dir', type=str, default=data_dir) # Dataset folder
 
     # Parameters for meta-train phase
