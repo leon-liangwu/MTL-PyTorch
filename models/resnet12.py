28e912aed20095c1ed21dfe75029949e75d23f7d
leon-liangwu <leon.liangwu@gmail.com>
2020-06-03 19:55:57 +0800

    resnet 12

--- models/resnet12.py
+++ models/resnet12.py
@@ -2,67 +2,25 @@ import torch.nn as nn
 from models.conv2d_mtl import Conv2dMtl
 # from conv2d_mtl import Conv2dMtl
 
-def conv3x3(in_planes, out_planes, stride=1, bias=False):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=bias)
-
-class BasicBlock(nn.Module):
-
-    def __init__(self, inplanes, planes, stride=1, drop_rate=0):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, bias=True)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.LeakyReLU(0.1)
-        self.conv2 = conv3x3(planes, planes, bias=True)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = conv3x3(planes, planes, bias=True)
-        self.bn3 = nn.BatchNorm2d(planes)
-        self.res_conv = nn.Conv2d(inplanes, planes, kernel_size=1, stride=1, bias=False)
-        self.maxpool = nn.MaxPool2d(2)
-
-        self.drop_rate = drop_rate
-        self.dropout = nn.Dropout(drop_rate)
-
-
-    def forward(self, x):
-        res = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-        out = self.relu(out)
-
-        res = self.res_conv(res)
-        out = out + res
-        out = self.maxpool(out)
-
-        out = self.dropout(out)
-
-        return out
-
-
-def conv3x3mtl(in_planes, out_planes, stride=1, bias=False):
-    return Conv2dMtl(in_planes, out_planes, kernel_size=3, stride=stride,
+def conv3x3(in_planes, out_planes, stride=1, bias=False, mtl=False):
+    if not mtl:
+        return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                        padding=1, bias=bias)
+    else:
+        return Conv2dMtl(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=bias)
 
 
 class BasicBlockMtl(nn.Module):
 
-    def __init__(self, inplanes, planes, stride=1, drop_rate=0):
+    def __init__(self, inplanes, planes, stride=1, drop_rate=0, mtl=False):
         super(BasicBlockMtl, self).__init__()
-        self.conv1 = conv3x3mtl(inplanes, planes, bias=True)
+        self.conv1 = conv3x3(inplanes, planes, mtl=mtl)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.LeakyReLU(0.1)
-        self.conv2 = conv3x3mtl(planes, planes, bias=True)
+        self.conv2 = conv3x3(planes, planes, mtl=mtl)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = conv3x3mtl(planes, planes, bias=True)
+        self.conv3 = conv3x3(planes, planes, mtl=mtl)
         self.bn3 = nn.BatchNorm2d(planes)
         self.res_conv = nn.Conv2d(inplanes, planes, kernel_size=1, stride=1, bias=False)
         self.maxpool = nn.MaxPool2d(2)
@@ -97,20 +55,25 @@ class BasicBlockMtl(nn.Module):
 
 class ResNet12Mtl(nn.Module):
 
-    def __init__(self, drop_rate=0.1, mtl=True):
+    def __init__(self, drop_rate=0.1, mtl=False):
         super(ResNet12Mtl, self).__init__()
-        if mtl:
-            block = BasicBlockMtl
-        else:
-            block = BasicBlock
 
-        channel = [64, 128, 256, 512]
+        block = BasicBlockMtl
+
+        channel = [64, 160, 320, 640]
         self.layer1 = block(3, channel[0], drop_rate)
-        self.layer2 = block(channel[0], channel[1], drop_rate)
-        self.layer3 = block(channel[1], channel[2], drop_rate)
-        self.layer4 = block(channel[2], channel[3], drop_rate)
+        self.layer2 = block(channel[0], channel[1], drop_rate, mtl=mtl)
+        self.layer3 = block(channel[1], channel[2], drop_rate, mtl=mtl)
+        self.layer4 = block(channel[2], channel[3], drop_rate, mtl=mtl)
 
         self.avgpool = nn.AvgPool2d(5, stride=1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
 
     def forward(self, x):
